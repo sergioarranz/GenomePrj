180a7f59f139a0c395c54df561b882b81cbe7e16
Sergio Arranz Sobrino <sergio.arranz@live.u-tad.com>
2017-11-09 19:23:58 +0100

    Tercer commit prueba funcionamiento remoto con Xcode

--- GenomePrj/MainCameraVC.swift
+++ GenomePrj/MainCameraVC.swift
@@ -25,7 +25,13 @@ class MainCameraVC: CameraViewController {
     
 
     /*
-    Hola
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
     */
 
 }
